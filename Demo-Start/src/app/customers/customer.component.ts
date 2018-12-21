65948d97e63a9c92156f801035d8c064521ed878
fpmr96a <softwaresolutions@cox.net>
2018-12-21 14:03:46 -0500

    added validation in component

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from'@angular/forms';
+import { FormGroup, FormBuilder, Validators } from'@angular/forms';
 
 import { Customer } from './customer';
 
@@ -12,15 +12,16 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
-
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['',  [Validators.required, Validators.maxLength(50)]],
+      email: ['',     [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog: true
     });
   }
 
@@ -37,5 +38,15 @@ export class CustomerComponent implements OnInit {
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 }
